594e3f091a92ced0006dc20f6ee2192a6750c302
imrulkayessifat <kayesimrul224@gmail.com>
2024-08-30 21:24:05 +0600

    refactor

--- hooks/ticket/use-get-tickets.ts
+++ hooks/ticket/use-get-tickets.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { getAllTickets } from "@/actions/get-tickets";
 
 interface UseGetTickets {
     token: string;
@@ -7,21 +8,7 @@ interface UseGetTickets {
 export const useGetTickets = ({token}:{token:string}) => {
     const query = useQuery({
         queryKey: ["get_tickets"],
-        queryFn: async () => {
-            const res = await fetch(`${process.env.NEXT_PUBLIC_MQSERVER}/ticket`,{
-                method: "GET",
-                headers: {
-                    "Authorization": `Bearer ${token}`, 
-                    "Content-Type": "application/json",
-                },
-            });
-            if (!res.ok) {
-                throw new Error("Failed to fetch images");
-            }
-            const  data  = await res.json();
-
-            return data;
-        },
+        queryFn: ()=>getAllTickets({token})
     })
     return query;
 }
