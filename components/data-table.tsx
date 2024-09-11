6cbb8bd9aec2f6295dbe713c98321914010f384b
imrulkayessifat <kayesimrul224@gmail.com>
2024-09-11 16:39:22 +0600

    design fix

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -234,9 +234,9 @@ export function DataTable<TData, TValue>({
             </div>
             <div className="flex items-center justify-end space-x-2 py-4">
                 <div className="flex-1 text-sm text-muted-foreground">
-                    {table.getFilteredSelectedRowModel().rows.length} of{" "}
-                    {table.getFilteredRowModel().rows.length} row(s) selected.
-                    {table.getRowCount()}
+                    Showing {table.getRowModel().rows.length} of {data.length} record(s)
+                    {/* {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                    {table.getFilteredRowModel().rows.length} row(s) selected. */}
                 </div>
                 <Button
                     variant="outline"
