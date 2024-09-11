1d769e22a9d503e1ba2ee30b44d44f369afea7ea
imrulkayessifat <kayesimrul224@gmail.com>
2024-09-11 12:47:47 +0600

    config issue

--- next.config.mjs
+++ next.config.mjs
@@ -10,27 +10,28 @@ const nextConfig = {
     },
     async rewrites() {
         return [
-            {
-                source: '/backend/:path*',
-                destination: 'http://localhost:3007/:path*',
-            },
+          {
+            source: '/backend/:path*',
+            destination: 'http://localhost:3007/:path*',
+          },
         ];
-    },
-    async headers() {
+      },
+      async headers() {
         return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'X-Forwarded-Proto',
-                        value: 'https',
-                    },
-                ],
-            },
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'X-Forwarded-Proto',
+                value: 'https',
+              },
+            ],
+          },
         ];
-    },
-    output: 'standalone',
-    trailingSlash: true,
+      },
+      output: 'standalone',
+      poweredByHeader: false,
+      reactStrictMode: true,
 };
 
 export default nextConfig;
